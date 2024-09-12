39af89d87ca71af48aeaee5b291f9e8410d2f772
dchan210 <dchan4@villanova.edu>
2024-09-11 21:46:52 -0400

    Delete js directory

--- js/name.js
+++
@@ -1,9 +0,0 @@
-const welcomeMessage = document.getElementById('welcome-header');
-console.log(welcomeMessage.textContent);
-
-const nameButton = document.getElementById('add-name');
-const title = document.getElementById('welcome-header');
-nameButton.addEventListener('click', function () {
-    const name = prompt("Please enter your name: ");
-    title.textContent = "Welcome, " + name + "!";
-});
